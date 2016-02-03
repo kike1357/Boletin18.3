6ad4cd62aaf8d5f595c9ac5f0252f881e18adf6e
epastorizalorenzo <epastorizalorenzo@atanasoff25.danielcastelao.org>
2016-02-03 10:51:47 +0100

    commit github

---
+++ src/boletin18/pkg3/Boletin183.java
@@ -0,0 +1,20 @@
+package boletin18.pkg3;
+
+/**
+ *
+ * @author epastorizalorenzo
+ */
+public class Boletin183 {
+
+    
+    public static void main(String[] args) {
+        Notas obj = new Notas();
+        obj.notas();
+        System.out.println("\n");
+        obj.ordenarNA();
+        obj.visNA();
+    }
+    }
+    
+    
+
